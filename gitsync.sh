69d11d099ddead6e2ef00646ae02cada9f6ab197
Perry Werneck <perry.werneck@gmail.com>
2019-02-25 09:26:01 -0300

    Adding sync script.

---
+++ gitsync.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# https://help.github.com/articles/syncing-a-fork/
+#
+# https://help.github.com/articles/configuring-a-remote-for-a-fork/
+#
+# https://www.opentechguides.com/how-to/article/git/177/git-sync-repos.html
+#
+# Setup:
+#
+# git remote add github https://github.com/PerryWerneck/pw3270-plugin-hllapi.git
+#
+#
+
+git push
+
+git fetch origin
+git checkout master
+git merge origin/master
+
+for repo in $(git remote -v | grep -v origin | grep "(push)" | awk '{print $1}')
+do
+	echo "Updating ${repo} ..."
+	git push ${repo}
+done
+
